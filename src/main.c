0aced8d8cca620aef5256b93d0745ad7a52789ee
Matthew <matthewgallan@gmail.com>
2024-02-26 18:44:26 +0000

    renamed shader files

--- src/main.c
+++ src/main.c
@@ -169,8 +169,8 @@ int main(int argc, char *argv[])
     GLuint shaders[2];
     size_t shaderCount = 2;
 
-    char *vsSource = readResource("shaders/shader.vs", app);
-    char *fsSource = readResource("shaders/shader.fs", app);
+    char *vsSource = readResource("shaders/fragment.glsl", app);
+    char *fsSource = readResource("shaders/vertex.glsl", app);
 
     if(vsSource == NULL || fsSource == NULL)
         return closeApp(app, 1);
